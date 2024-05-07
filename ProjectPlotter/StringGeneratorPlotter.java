b0de5a712ec49e924cda1a076db9c9652cbe1cd3
shujiejune <137582207+shujiejune@users.noreply.github.com>
2024-05-07 06:17:21 -0700

    Update StringGeneratorPlotter.java
    Add a new function to calculate problem size from in.txt files to replace hardcode.

--- ProjectPlotter/StringGeneratorPlotter.java
+++ ProjectPlotter/StringGeneratorPlotter.java
@@ -1,3 +1,4 @@
+import org.apache.commons.lang3.math.NumberUtils;
 import org.knowm.xchart.BitmapEncoder;
 import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.SwingWrapper;
@@ -7,25 +8,24 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class StringGeneratorPlotter {
 
-    private static double[] timeResults = new double[15];
-    private static double[] memoryReuslts = new double[15];
+    private static final int numFiles = 15;
+    private static final double[] timeResults = new double[15];
+    private static final double[] memoryReuslts = new double[15];
 
     public static void getResults() {
-        int numFiles = 15;
-        ArrayList<Double> yResults = new ArrayList<>();
-
         for (int i = 0; i < numFiles; i++) {
             String timeUsage;
             String memUsage;
-            File file = (new File(System.getProperty("user.dir"), "/out" + (i + 1) + ".txt"));
+            File output = new File(System.getProperty("user.dir"), "/out" + (i + 1) + ".txt");
             try {
-                timeUsage = (String) Files.readAllLines(Path.of(file.getAbsolutePath())).get(3);
-                memUsage = (String) Files.readAllLines(Path.of(file.getAbsolutePath())).get(4);
+                timeUsage = Files.readAllLines(Path.of(output.getAbsolutePath())).get(3);
+                memUsage = Files.readAllLines(Path.of(output.getAbsolutePath())).get(4);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -34,27 +34,50 @@ public class StringGeneratorPlotter {
         }
     }
 
+    public static double[] getProblemSizes() {
+        double[] sizes = new double[numFiles];
+
+        List<String> Lines;
+
+        for (int i = 0; i < numFiles; i++) {
+            int baseLen1, baseLen2 = 0;
+            int step1 = 0, step2 = 0;
+            int flag = 1;
+            File input = new File(System.getProperty("user.dir"), "/in" + (i + 1) + ".txt");
+            try {
+                Lines = Files.readAllLines(Path.of(input.getAbsolutePath()));
+                baseLen1 = Lines.get(0).length();
+                for (int j = 1; j < Lines.size(); j++) {
+                    if (NumberUtils.isParsable(Lines.get(j))) {
+                        if (flag == 1) {
+                            step1++;
+                        }
+                        else {
+                            step2++;
+                        }
+                    }
+                    else {
+                        baseLen2 = Lines.get(j).length();
+                        flag = 2;
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            sizes[i] = (double) baseLen1 * Math.pow(2, step1) + baseLen2 * Math.pow(2, step2);
+        }
+
+        return sizes;
+    }
+
     public static void main(String[] args) {
-        double[] xData = new double[] {
-                16.0,
-                64.0,
-                128.0,
-                256.0,
-                384.0,
-                512.0,
-                768.0,
-                1024.0,
-                1280.0,
-                1536.0,
-                2048.0,
-                2560.0,
-                3072.0,
-                3584.0,
-                3968.0
-        };
+        double[] xData = getProblemSizes();
+        //System.out.println(Arrays.toString(xData));
         getResults();
-        XYChart timeChart = QuickChart.getChart("Input Size Versus Time", "M+N", "Time in MS", "Efficient", xData, timeResults);
-        XYChart memoryChart = QuickChart.getChart("Input Size Versus Memory", "M+N", "Memory in KB", "Efficient", xData, memoryReuslts);
+        XYChart timeChart = QuickChart.getChart("Input Size Versus Time",
+                "M+N", "Time in MS", "Efficient", xData, timeResults);
+        XYChart memoryChart = QuickChart.getChart("Input Size Versus Memory",
+                "M+N", "Memory in KB", "Efficient", xData, memoryReuslts);
 
         new SwingWrapper(timeChart).displayChart();
         new SwingWrapper(memoryChart).displayChart();
