201b39f0bf2ebc51c25d35317d0bf071ac263694
Rararahul <58281823+rkatwala@users.noreply.github.com>
2024-05-07 19:41:17 -0700

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,6 +1,6 @@
-python3 test.py input1.txt output1.txt
-python3 test.py input2.txt output2.txt
-python3 test.py input3.txt output3.txt
-python3 test.py input4.txt output4.txt
-python3 test.py input5.txt output5.txt
+python3 basic_3.py input1.txt output1.txt
+python3 basic_3.py input2.txt output2.txt
+python3 basic_3.py input3.txt output3.txt
+python3 basic_3.py input4.txt output4.txt
+python3 basic_3.py input5.txt output5.txt
 
