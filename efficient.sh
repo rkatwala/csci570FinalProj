b0d69015fc60a76925331c44a0744491b00134e2
shujiejune <137582207+shujiejune@users.noreply.github.com>
2024-05-08 18:33:55 -0700

    Update efficient.sh

--- efficient.sh
+++ efficient.sh
@@ -1,16 +1,2 @@
 javac Efficient.java
-java Efficient in1.txt out1.txt
-java Efficient in2.txt out2.txt
-java Efficient in3.txt out3.txt
-java Efficient in4.txt out4.txt
-java Efficient in5.txt out5.txt
-java Efficient in6.txt out6.txt
-java Efficient in7.txt out7.txt
-java Efficient in8.txt out8.txt
-java Efficient in9.txt out9.txt
-java Efficient in10.txt out10.txt
-java Efficient in11.txt out11.txt
-java Efficient in12.txt out12.txt
-java Efficient in13.txt out13.txt
-java Efficient in14.txt out14.txt
-java Efficient in15.txt out15.txt
+java Efficient "$1" "$2"
