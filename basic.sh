814d7f2a27f454a14dd51117ed6d23ee5299ee1b
erniemeister2016 <epimentel2016@gmail.com>
2024-05-08 18:31:46 -0700

    Updated basic.sh to accept any argument

--- basic.sh
+++ basic.sh
@@ -1,6 +1,1 @@
-python3 basic_3.py input1.txt output1.txt
-python3 basic_3.py input2.txt output2.txt
-python3 basic_3.py input3.txt output3.txt
-python3 basic_3.py input4.txt output4.txt
-python3 basic_3.py input5.txt output5.txt
-
+python3 basic_3.py "$1" "$2"
